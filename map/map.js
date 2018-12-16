9cebbbcece47348b3a0c091545bad1075d17c665
Nougaillac <44436584+Nougaillac@users.noreply.github.com>
2018-12-16 21:41:08 +0100

    Modif carte_sav avec session storage
    Ajout du session storage pour enregistrer le tableau locations
    Sans ça les clusters ne s'affichent pas

--- map/map.js
+++ map/map.js
@@ -1,79 +1,105 @@
-var locations = [];
+function initMap() {
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 2,
+            center: {lat: 37.024, lng: -100.887}
+        });
+
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function (location, i) {
+            return new google.maps.Marker({
+                position: location,
+                label: labels[i % labels.length]
+            });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'images/m'});
+    }
+
+// initialisation des variables date pour rechercher les films qui vont sortir dans les 15 prochains jours
 var date = new Date();
+var day = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
 var j15 = new Date(date.setDate(date.getDate()+15))
 var day15 = j15.getFullYear()+"-"+(j15.getMonth()+1)+"-"+j15.getDate()
 	
-//limite des 40 films => 2 pages
-for (var p = 1; p < 3; p++){	 
+
 			
-	//variables qui récupèrent les clés des API enregistrées dans config.js
-	var movie_key = config.MOVIE_KEY;
-	var map_key = config.MAP_KEY;
-	//recherche des films des 15 prochains jours
-	var url = 'https://api.themoviedb.org/3/discover/movie?primary_release_date.gte='+day+'&primary_release_date.lte='+day15+'&api_key='+movie_key;
+//variables qui récupèrent les clés des API enregistrées dans config.js
+var movie_key = config.MOVIE_KEY;
+var map_key = config.MAP_KEY;
 
-	fetch(url).then(function(response){
-		response.json().then(function(film){ 
-			console.log(film)
-	
-			//films sortis en 2018
-			for (var i = 0; i < film.results.length; i++){
+//initialisation des variables 
+let url_movie, i, idFilm, url_film, pays, url_paysFilm, map, infowindow, marker, locations;
+
+//recherche des films des 15 prochains jours
+url_movie = 'https://api.themoviedb.org/3/discover/movie?primary_release_date.gte='+ day + '&primary_release_date.lte='+ day15 + '&api_key='+movie_key;
+
+	fetch(url_movie).then(function (response) {
+	if (response.status === 200) {
+		
+		//Récupération des données json
+
+		response.json().then(function (film) {
+			//films à sortir les 15 prochains jours
+			for (i = 0; i < film.results.length; i++) {
+
 				//recherche du pays de production des films à partir de l'id
-				var id = film.results[i].id;
-				var url2 = 'https://api.themoviedb.org/3/movie/'+id+'?api_key='+movie_key;
+				idFilm = film.results[i].id;
 
-				fetch(url2).then(function(reponse){
-					reponse.json().then(function(id_film){ 
-						console.log(id_film)
-					
-						// test de l'existence du pays de production
-						if (id_film.hasOwnProperty('production_countries')){											
-							//recherche des coordonnées GPS du pays à partir de son code pays (US, DE,..) en utilisant l'api google geocode
-							var pays = id_film.production_countries[0].iso_3166_1;
-							var url3 = 'https://maps.googleapis.com/maps/api/geocode/json?key='+map_key+'&address='+pays+'&sensor=false'
+				url_film = 'https://api.themoviedb.org/3/movie/'+idFilm+'?api_key='+movie_key;
 
-							fetch(url3).then(function(resp){
-								resp.json().then(function(gps){
-									//remplissage du tableau locations contenant les coordonnées GPS (lat, lng)
-									locations.push({lat:gps.results[0].geometry.location.lat, lng:gps.results[0].geometry.location.lng});
-									console.log(locations);
-									return locations;
-								});											
-							});										
-						}
+				fetch(url_film).then(function (reponse) {
+					reponse.json().then(function (id_film) {
+						
+						//recherche des coordonnées GPS du pays à partir de son code pays (US, DE,..) en utilisant l'api google geocode
+						pays = id_film.production_countries[0].iso_3166_1;
+						url_paysFilm = 'https://maps.googleapis.com/maps/api/geocode/json?address='+pays+'&key='+map_key+'&sensor=false';
+
+						//initialisation de la session storage pour locations
+                        if (sessionStorage.getItem('initStorage') !== 'true') {
+							locations = [];
+							sessionStorage.setItem('locations', locations);
+							sessionStorage.setItem('initStorage', 'true');
+							sessionStorage.setItem('initStorage', 'false');
+                        }
+
+						fetch(url_paysFilm).then(function (resp) {
+							resp.json().then(function (gps) {
+								//remplissage du tableau locations contenant les coordonnées GPS (lat, lng)
+								locations.push({
+									lat: gps.results[0].geometry.location.lat,
+									lng: gps.results[0].geometry.location.lng
+								});
+								// le problème avec cette suite de fetch est d'extraire locations d'où le sessionStorage
+								sessionStorage.setItem('locations', JSON.stringify(locations));
+								sessionStorage.setItem('initStorage', 'true');
+								sessionStorage.setItem('dataStorage', 'true');
+								
+								console.log(url_paysFilm);
+								console.log(locations);
+								console.log('---------------');
+							});
+						});
 					});
-				});						
+				});
 			}
 		});
-	});
+	}
+});
+
+//Si pas de sessionStorge valide faire F5
+if (sessionStorage.getItem('dataStorage') === 'true') {
+	locations = JSON.parse(sessionStorage.getItem('locations'));
+
+} else {
+	alert('Refresher the page with F5');
+
 }
-
-function initMap() {
-	//console.log(locations)
-	var map = new google.maps.Map(document.getElementById('map'), {
-	zoom: 3,
-	center: {lat: 48.85, lng: 2.34}
-});
-
-// Create an array of alphabetical characters used to label the markers.
-var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-// Add some markers to the map.
-// Note: The code uses the JavaScript Array.prototype.map() method to
-// create an array of markers based on a given "locations" array.
-// The map() method here has nothing to do with the Google Maps API.
-var markers = locations.map(function(location, i) {
-	return new google.maps.Marker({
-		position: location,
-		label: labels[i % labels.length]
-	});
-});
-
-// Add a marker clusterer to manage the markers.
-var markerCluster = new MarkerClusterer(map, markers,
-				{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js'});
-		}
-		
-//appel de la fonction de création des markers
-src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"
-		
