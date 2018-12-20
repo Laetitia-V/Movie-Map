7e8ab269c4ca86a00c5a9c165ecb309be1a2fe4f
Laetitia-V <laetitia.viau@hotmail.fr>
2018-12-20 17:49:16 +0100

    Version définitive

--- dom.js
+++
@@ -1,6 +0,0 @@
-// document.querySelector("#fieldWho") //sélectionne l’élément de id fieldWho
-// document.querySelectorAll(".buttonForm") // sélectionne tous les éléments de class buttonForm
-
-// const li = document.createElement('li') // crée un élément de type li ( <li></li>)
-
-// li.innerHTML = “toto” // insère “toto” dans l’élément (<li>toto</li>)
